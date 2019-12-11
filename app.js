7444ceee73888593c2aa005b1741fcd3386196b4
Ivan <ivan.rylskiy@gmail.com>
2019-12-11 21:25:33 +0700

    connecting with front

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-// const express = require('express');
-// const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// const tasksRoutes = require('./routes/tasks');
-
-// // app.use(morgan('combined'));
-// app.use(bodyParser.json()); // application/json
-
-// app.use('/', (req, res, next) => {
-//     console.log(req.headers);
-//     next();
-// });
-
-// app.use('/', (req, res, next) => {
-//     console.log('middleware 2');
-//     next();
-// });
-
-// // app.get('/', (req, res) => {
-// //     res.send('Hello JS');
-// // });
-
-// app.use('/tasks', tasksRoutes);
-
-// app.use((err, req, res, next) => {
-//     const { message } = err;
-//     res.json({ status: 'ERROR', message });
-// });
-
-// app.listen(8080, () => console.log('listening on port 8080'));
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -49,6 +16,12 @@ app.use((req, res, next) => {
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
+});
+
+app.use((req, res, next) => {
+  setTimeout(() => {
+    next();
+  }, 500);
 });
 
 app.use('/accounts', accountsRotues);
